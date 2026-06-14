285a3a2c9bf67dc97cc374461d860453afd572ad
mazaherireza <rezamazaheri.email@gmail.com>
2026-06-14 12:08:08 +0330

    Add Presentation & Memories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import { type ReactNode } from "react";
 
+import PresentationComponnet from "@/components/presentation/presentation.component";
+import MemoriesComponent from "@/components/memories/memories.component";
+
 import styles from "./page.module.css";
 
 export default function Home(): ReactNode {
   return (
     <div className={styles.page}>
-      <h1>هما روستا</h1>
+      <PresentationComponnet />
+      <MemoriesComponent />
     </div>
   );
 }
