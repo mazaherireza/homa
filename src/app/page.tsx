dbdf7096669bba046d69f6f9f9946411eb4b7133
mazaherireza <rezamazaheri.email@gmail.com>
2026-06-16 08:56:47 +0330

    Remove Code Which Made Error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,6 @@ import MemoriesComponent from "@/components/memories/memories.component";
 import styles from "./page.module.css";
 
 export default function Page(): ReactNode {
-  throw new Error("KHATA");
-
   return (
     <div className={styles.page}>
       <PresentationComponnet />
